feeaff17f5a7af1aceb89750c484d9111de452a2
Bihan-Codes <bihanbca@gmail.com>
2026-03-17 11:39:12 +0530

    Railway Connected

--- db.js
+++ db.js
@@ -1,18 +1,24 @@
-const mysql = require ('mysql2')
+require("dotenv").config({path : "docs.env"});
+
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host : "localhost",
-    user: "root",
-    password : "Root@123",
-    database : "project"
-})
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-db.connect((err)=>{
-    if (err){
-        console.error("Database Connection failed: ", err)
-        return
-    }
-    console.log("MySQL Connected!")
-})
+db.connect((err) => {
+  if (err) {
+    console.error("Database Connection failed:", err);
+    return;
+  }
+  console.log("MySQL Connected! 🚀");
+});
 
-module.exports = db
+module.exports = db;
