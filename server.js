73bc11ab532fdf600a20308f4b7dc0473be982a2
Bihan-Codes <bihanbca@gmail.com>
2026-03-18 11:44:06 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const db = require("./db");
 
 const app = express();
 
+
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
 // 🔥 MIDDLEWARE (CLEAN ORDER)
 app.use(cors());
 app.use(express.json());
@@ -128,3 +133,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server Started on port ${PORT}`);
 });
+
+
+setInterval(() => {
+    console.log("Server Alive...");
+}, 25000);
